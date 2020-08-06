d34a2b4a52c980ce62f1e0df5fe9661bdd1bab0a
Vitamin <vitamincarrot0622@gmail.com>
2020-08-07 07:49:17 +0900

    전화번호 리사이클러뷰 생성 여러 전화번호로 전송 가능 권한 허용 여부 확인 권한 여부에 따라 전송 버튼 보이기

--- app/src/main/java/com/example/groupsms/MainActivity.java
+++ app/src/main/java/com/example/groupsms/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.groupsms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import jxl.Workbook;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText num;
     EditText msg;
+    Button sendBtn;
+    Button retryBtn;
+    Button addBtn;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    ArrayList<String> myDataset = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +38,118 @@ public class MainActivity extends AppCompatActivity {
 
         num = findViewById(R.id.phonenumber);
         msg = findViewById(R.id.message);
-        Button sendbtn = findViewById(R.id.sendbtn);
+        sendBtn = findViewById(R.id.sendbtn);
+        retryBtn = findViewById(R.id.retrybtn);
+        addBtn = findViewById(R.id.add);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
-        sendbtn.setOnClickListener(new View.OnClickListener() {
+
+        checkForSmsPermission();
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inputText = num.getText().toString();
-                String inputText2 = msg.getText().toString();
-                if (inputText.length() > 0 && inputText2.length() > 0) {
-                    sendSMS(inputText,inputText2);
-                    Toast.makeText(getBaseContext(),inputText + "\n"+inputText2,Toast.LENGTH_SHORT).show();
 
-                }
-                else {
-                    Toast.makeText(getBaseContext(), "Enter the PhoneNumber and Message!", Toast.LENGTH_SHORT).show();
+                myDataset.add(num.getText().toString());
+                Log.d("data", "Data add : " + myDataset.get(0));
+
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputMsgText = msg.getText().toString();
+                if (myDataset.size() > 0 && inputMsgText.length() > 0) {
+                    for (int i = 0; i < myDataset.size(); i++) {
+                        sendSMS(myDataset.get(i), inputMsgText);
+                        toastMsgShort(myDataset.get(i) + "\n" + inputMsgText);
+
+                    }
+
+                } else {
+                    toastMsgLong("Enter the PhoneNumber and Message!");
                 }
 
             }
         });
+
+        retryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkForSmsPermission();
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MyAdapter adapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(adapter);
+
+
     }
 
-    private void sendSMS(String phonenum, String message){
+    private void sendSMS(String phoneNum, String message) {
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phonenum,null,message,null,null);
+        sms.sendTextMessage(phoneNum, null, message, null, null);
+    }
+
+    private void checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Log.d("checkself", "Dont grented!");
+            // Permission not yet granted. Use requestPermissions().
+            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+
+            Log.d("checkself", "" + Manifest.permission.SEND_SMS);
+
+        } else {
+            // Permission already granted. Enable the SMS button.
+            Log.d("checkself", "Grented!");
+            Log.d("checkself", "" + Manifest.permission.SEND_SMS);
+            enableSmsButton();
+        }
+    }
+
+    private void disableSmsButton() {
+        Toast.makeText(this, "SMS usage disabled", Toast.LENGTH_LONG).show();
+        sendBtn.setVisibility(View.INVISIBLE);
+        retryBtn.setVisibility(View.VISIBLE);
+    }
+
+    private void enableSmsButton() {
+        sendBtn.setVisibility(View.VISIBLE);
+        retryBtn.setVisibility(View.INVISIBLE);
+    }
+
+    private void toastMsgLong(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void toastMsgShort(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    // 권한을 요구시 어떤 버튼(허용, 거부)을 눌렀는지 응답을 받음.
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_SEND_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("checkself", "Permission has been granted");
+                enableSmsButton();
+            } else {
+                Log.d("checkself", "Permission has been denied or request cancelled");
+                disableSmsButton();
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/groupsms/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.groupsms;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private ArrayList<String> mDataset = null;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public MyViewHolder(View v) {
+            super(v);
+            textView = v.findViewById(R.id.adapterTextView);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.phonenumber_view, parent, false);
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        String text = mDataset.get(position);
+        holder.textView.setText(text);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
+
