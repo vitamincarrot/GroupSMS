7fb8921a4abb6dd306354528e5c1dea0f8216fde
NepaPC <vitamincarrot0622@gmail.com>
2020-08-10 20:28:30 +0900

    치환 텍스트 변경

--- app/src/main/java/com/example/groupsms/MainActivity.java
+++ app/src/main/java/com/example/groupsms/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.groupsms;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -14,22 +9,40 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText num;
-    EditText msg;
-    Button sendBtn;
-    Button retryBtn;
-    Button addBtn;
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+    EditText num, msg;
+    Button sendBtn, retryBtn, addBtn, checkDelBtn, inputButton1;
+    Button btnSDCard;
+    String inputText1 = "Text1";
+    RecyclerView recyclerView;
+    private String[] FilePathStrings;
+    private String[] FileNameStrings;
+    private File[] listFile;
+    File file;
+    /*public RecyclerView.Adapter mAdapter;
+    public RecyclerView.LayoutManager layoutManager;*/
     ArrayList<String> myDataset = new ArrayList<>();
+    ArrayList<String> checkText = new ArrayList<>();
+    ArrayList<String> pathHistory;
+    String lastDirectory;
+    int count = 0;
+    ListView lvInternalStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +54,12 @@ public class MainActivity extends AppCompatActivity {
         sendBtn = findViewById(R.id.sendbtn);
         retryBtn = findViewById(R.id.retrybtn);
         addBtn = findViewById(R.id.add);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerView);
+        checkDelBtn = findViewById(R.id.checkDel);
+        inputButton1 = findViewById(R.id.inputText1);
 
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         final MyAdapter adapter = new MyAdapter(myDataset);
@@ -52,18 +67,75 @@ public class MainActivity extends AppCompatActivity {
 
         checkForSmsPermission();
 
+        inputButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertSomeText(inputText1);
+            }
+        });
 
+        inputButton1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.d("MainActivity_Log", "(changeTextBtn1) Long Clicked.");
+                final LinearLayout linear = (LinearLayout) View.inflate(MainActivity.this, R.layout.dialog_buttontextchange, null);
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setView(linear)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText changeBtnText1 = linear.findViewById(R.id.changeEditText1);
+                                inputText1 = changeBtnText1.getText().toString();
+                                inputButton1.setText(inputText1);
+                                dialog.dismiss();
+                            }
+                        })
+
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+                return true;
+            }
+        });
+
+        checkDelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkText = adapter.checkPos;
+                for (int i = 0; i < checkText.size(); i++) {
+                    myDataset.remove(checkText.get(i));
+                }
+
+                adapter.checkPos.removeAll(adapter.checkPos);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+        //"+"버튼 클릭시 번호 리스트 추가
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                myDataset.add(num.getText().toString());
-                Log.d("data", "Data add : " + myDataset.get(0));
-                adapter.notifyDataSetChanged();
+                if (num.length() > 0) {
+                    myDataset.add(num.getText().toString());
+                    Log.d("MainActivity_Log", "(addBtn) Data add : " + myDataset.get(0));
+                    adapter.notifyDataSetChanged();
+                    num.getText().clear();
 
-                num.getText().clear();
+                } else {
+                    toastMsgShort("Enter the phonenumber!");
+
+                }
 
             }
+
+
         });
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
@@ -91,21 +163,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
 
+    private void insertSomeText(String st) {
+        int start = Math.max(msg.getSelectionStart(), 0);
+        int end = Math.max(msg.getSelectionEnd(), 0);
+        msg.getText().replace(Math.min(start, end), Math.max(start, end), st);
+    }
+
+
+    //SMS 메세지 보내기
     private void sendSMS(String phoneNum, String message) {
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNum, null, message, null, null);
     }
 
+    //SMS 보내기 권한 허용 했는지 확인
     private void checkForSmsPermission() {
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS) !=
                 PackageManager.PERMISSION_GRANTED) {
-            Log.d("checkself", "Dont grented!");
+            Log.d("MainActivity_Log", "(checkForSmsPermission) Dont grented!");
             // Permission not yet granted. Use requestPermissions().
             // MY_PERMISSIONS_REQUEST_SEND_SMS is an
             // app-defined int constant. The callback method gets the
@@ -114,31 +192,35 @@ public class MainActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.SEND_SMS},
                     MY_PERMISSIONS_REQUEST_SEND_SMS);
 
-            Log.d("checkself", "" + Manifest.permission.SEND_SMS);
+            Log.d("MainActivity_Log", "(checkForSmsPermission) " + Manifest.permission.SEND_SMS);
 
         } else {
             // Permission already granted. Enable the SMS button.
-            Log.d("checkself", "Grented!");
-            Log.d("checkself", "" + Manifest.permission.SEND_SMS);
+            Log.d("MainActivity_Log", "(checkForSmsPermission) Grented!");
+            Log.d("MainActivity_Log", "(checkForSmsPermission) " + Manifest.permission.SEND_SMS);
             enableSmsButton();
         }
     }
 
+    //Retry 버튼 보이게
     private void disableSmsButton() {
         Toast.makeText(this, "SMS usage disabled", Toast.LENGTH_LONG).show();
         sendBtn.setVisibility(View.INVISIBLE);
         retryBtn.setVisibility(View.VISIBLE);
     }
 
+    //Send 버튼 보이게
     private void enableSmsButton() {
         sendBtn.setVisibility(View.VISIBLE);
         retryBtn.setVisibility(View.INVISIBLE);
     }
 
+    //Toast 메세지 길게
     private void toastMsgLong(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 
+    //Toast 메세지 짧게
     private void toastMsgShort(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
@@ -148,10 +230,10 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == MY_PERMISSIONS_REQUEST_SEND_SMS) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("checkself", "Permission has been granted");
+                Log.d("MainActivity_Log", "(onRequestPermissionsResult) Permission has been granted");
                 enableSmsButton();
             } else {
-                Log.d("checkself", "Permission has been denied or request cancelled");
+                Log.d("MainActivity_Log", "(onRequestPermissionsResult) Permission has been denied or request cancelled");
                 disableSmsButton();
             }
         }
--- app/src/main/java/com/example/groupsms/MyAdapter.java
+++ app/src/main/java/com/example/groupsms/MyAdapter.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -15,16 +14,13 @@ import java.util.ArrayList;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private ArrayList<String> mDataset = null;
-    private ArrayList<String> checkPos = new ArrayList<>();
+    private ArrayList<String> mDataSet;
+    public ArrayList<String> checkPos = new ArrayList<>();
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+
         public TextView textView;
-        public int pos;
         public CheckBox checkBox;
 
         public MyViewHolder(View v) {
@@ -33,31 +29,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             checkBox = v.findViewById(R.id.checkBox);
 
 
-           /* v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.d("myadapter","Clicked!");
-
-                    pos = getAdapterPosition();
-                    if (pos != RecyclerView.NO_POSITION){
-                        Log.d("myadapter","Position NO : " + pos);
-
-                    }
-                }
-            });*/
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public MyAdapter(ArrayList<String> myDataset) {
-        mDataset = myDataset;
+        mDataSet = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
+
     @Override
     public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
-        // create a new view
         Context context = parent.getContext();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.phonenumber_view, parent, false);
@@ -65,32 +47,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        String text = mDataset.get(position);
+        String text = mDataSet.get(position);
+        if (checkPos.size() == 0) {
+            holder.checkBox.setChecked(false);
+
+        }
+
         holder.textView.setText(text);
         holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    checkPos.add("" + position);
-                    Log.d("myadapter", "Add delete data position : " + position);
-                } else {
-                    checkPos.remove("" + position);
-                    Log.d("myadapter", "Remove delete data position : " + position);
+
+                if (mDataSet.size() > 0) {
+                    if (isChecked) {
+                        checkPos.add(mDataSet.get(position));
+                        Log.d("MyAdapter_Log", "(onBindViewHolder) Add delete data position : " + mDataSet.get(position));
+                    } else {
+                        checkPos.remove(mDataSet.get(position));
+                        Log.d("MyAdapter_Log", "(onBindViewHolder) Remove delete data position : " + mDataSet.get(position));
+                    }
                 }
+
             }
+
+
         });
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return mDataSet.size();
     }
 }
 
